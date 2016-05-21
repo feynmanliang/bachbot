f8e153417783dfcb60f913ee4643b96f4a61574e
Feynman Liang <feynman.liang@gmail.com>
2016-05-22 00:47:49 +0100

    Fixes  tokenization script path

--- scripts/2-tokenize.zsh
+++ scripts/2-tokenize.zsh
@@ -1,9 +1,9 @@
-#!/usr/bin/zsh
+#!/usr/bin/env zsh
 
 COMPOSER=Bach+Johann
 
 # preprocess data (tokenize store in hdf5)
-python torch-rnn/scripts/preprocess.py \
+python ~/torch-rnn/scripts/preprocess.py \
     --input_txt scratch/${COMPOSER}.txt \
     --output_h5 scratch/${COMPOSER}.h5 \
     --output_json scratch/${COMPOSER}.json
