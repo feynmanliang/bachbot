f7788bb9ffc8961a5a73081d6377c13a180eb376
Feynman Liang <feynman.liang@gmail.com>
2016-05-22 15:05:23 +0100

    Training grid search

--- scripts/3-train.zsh
+++ scripts/3-train.zsh
@@ -2,9 +2,30 @@
 
 COMPOSER=Bach+Johann
 
+wordvec_size=64
+num_layers=4
+rnn_size=256
+
 # train model
 cd ~/torch-rnn
 SCRATCH_DIR=~/bachbot/scratch
-th train.lua \
-    -input_h5 ${SCRATCH_DIR}/${COMPOSER}.h5 \
-    -input_json ${SCRATCH_DIR}/${COMPOSER}.json
+
+for wordvec_size in 16 32 64 128; do
+    for num_layers in 1 2 3 4; do
+        for rnn_size in 128 256 512; do
+            fname="wordvec=${wordvec_size},num_layers=${num_layers},rnn_size=${rnn_size}"
+            print $fname
+            th train.lua \
+                -input_h5 ${SCRATCH_DIR}/${COMPOSER}.h5 \
+                -input_json ${SCRATCH_DIR}/${COMPOSER}.json \
+                -wordvec_size $wordvec_size \
+                -rnn_size $rnn_size \
+                -num_layers $num_layers \
+                -checkpoint_name $fname/checkpoint \
+                -print_every 50 \
+                -checkpoint_every 1000 \
+                -gpu_backend cuda
+        done
+    done
+done
+
