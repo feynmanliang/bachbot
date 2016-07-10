5c89689ee1df2627c6aabeb069382d7acf842780
Feynman Liang <feynman.liang@gmail.com>
2016-07-10 14:13:02 +0100

    Prepares constant time polyphonic

--- scripts/constants.py
+++ scripts/constants.py
@@ -11,4 +11,7 @@ END_DELIM = unichr(1115)
 
 NOTE_START_SYM = '@'
 
+# quarterLength's in JCB chorales:
+# [0.5, 1.0, 2.0, 3.0, 4.0, 6.0, 0.25, 8.0, 12.0, 0.125, 0.75, 1.5]
+# Multiplying by 8 turns all durations into integers
 FRAMES_PER_CROTCHET = 8
