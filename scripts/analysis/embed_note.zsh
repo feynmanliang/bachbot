12b0270084ed9edeac9665413294143ddfc0ad8e
Feynman Liang <feynman.liang@gmail.com>
2016-07-18 21:49:39 +0100

    Multiple chord embeddings

--- scripts/analysis/embed_note.zsh
+++ scripts/analysis/embed_note.zsh
@@ -2,8 +2,11 @@
 
 cd ~/torch-rnn
 
+mkdir -p $2
+
 th ./embed_note.lua \
   -checkpoint ~/torch-rnn/checkpoints/seq_length=64,wordvec=64,num_layers=2,rnn_size=128,dropout=0,batchnorm=1,lr=2e-3/checkpoint_14000.t7 \
   -embed_text_file $1 \
+  -out_dir $2 \
   -verbose 1 \
   -gpu 1
